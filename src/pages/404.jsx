0760291390b03006e07f2b0c9199dd9ab081ad84
Abinash Karmakar <codeAbinash@gmail.com>
2022-07-01 21:10:07 +0530

    Texting Links

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -8,8 +8,8 @@ import '../styles/default/404.scss'
 export default function NotFound() {
     return (
         <div className='wrongLink'>
-            <h1>Change your Settings for browser Support</h1>
-            <p>This is unavailable because of This React Application is deployed on Github Pages</p>
+            <h1>404 : Page Not Found</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, explicabo.</p>
             <Link to={repoPath('/home')}>
                 <button>Go to main page </button>
             </Link>
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,5 +1,12 @@
-export default function Start(){
-    return(
-        <h1>Abinash</h1>
-    )   
+import { repoPath } from "../js/deploy";
+import { Link } from "react-router-dom";
+
+export default function Start() {
+    return (
+        <>
+
+            <h1>Abinash</h1>
+            <Link to={repoPath('/set-location')}>Set Location</Link>
+        </>
+    )
 }
