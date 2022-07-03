4c1de048efd37fba86c161af22f0e93b26113fa7
Abinash Karmakar <codeAbinash@gmail.com>
2022-07-03 17:40:29 +0530

    Search City Screen Design

--- src/pages/SetLocation.jsx
+++ src/pages/SetLocation.jsx
@@ -2,13 +2,68 @@ import '../styles/pages/screen/SetLocation.scss'
 
 //Components
 import Header from '../components/header/Header'
+import BlueButton from '../components/buttons/BlueButton'
 
 
 
-export default function SetLocation(){
-   return(
+//images
+// import locationImage from '../images/illustrations/location_pink.png'
+import backImage from '../images/icons/back.svg'
+
+
+export default function SetLocation() {
+
+   return (
       <div className='setLocation'>
-         <Header text="Search Your Location" back={true}/>
+         <div className="topDiv">
+            <Header text="Search Your Location" back={ true } />
+            <div className="searchDiv">
+               <input type="text" placeholder='Search Your City' value='Bankura'/>
+            </div>
+            {/* <img src={ locationImage } alt="Location" /> */ }
+            <div className="cities">
+               <p className='countResult'>4 Results Found for "Bankura"</p>
+               <div className="city tap2">
+                  <div className="left">
+                     <p className="name">Bankura, <span className='smallText'>West Bengal, India</span></p>
+                  </div>
+                  <div className="right">
+                     <img src={ backImage } alt="Forward" />
+                  </div>
+               </div>
+               <div className="city tap2">
+                  <div className="left">
+                     <p className="name">Bankura, <span className='smallText'>Jharkhand, India</span></p>
+                  </div>
+                  <div className="right">
+                     <img src={ backImage } alt="Forward" />
+                  </div>
+               </div>
+               <div className="city tap2">
+                  <div className="left">
+                     <p className="name">Bankura, <span className='smallText'>Rajshahi, Bangladesh</span></p>
+                  </div>
+                  <div className="right">
+                     <img src={ backImage } alt="Forward" />
+                  </div>
+               </div>
+               <div className="city tap2">
+                  <div className="left">
+                     <p className="name">Bankura, <span className='smallText'>West Bengal, India</span></p>
+                  </div>
+                  <div className="right">
+                     <img src={ backImage } alt="Forward" />
+                  </div>
+               </div>
+
+            </div>
+         </div>
+         <section>
+            <div className="bottomDiv">
+               <BlueButton text="Use GPS" />
+               <p>Turn on Location to Locate</p>
+            </div>
+         </section>
 
 
       </div>
